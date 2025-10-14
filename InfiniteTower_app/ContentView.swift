8ffd8c918dc72558d7a508995e46e298854a5c9e
gabemaymi-creator <gabemaymi@gmail.com>
2025-10-14 09:33:12 -0400

    Initial Commit

---
+++ InfiniteTower_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfiniteTower_app
+//
+//  Created by student05 on 10/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfiniteTower_app/InfiniteTower_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteTower_appApp.swift
+//  InfiniteTower_app
+//
+//  Created by student05 on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteTower_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
